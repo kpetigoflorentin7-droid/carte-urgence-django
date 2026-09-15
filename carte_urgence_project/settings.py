ba0a6836ae773be69bab27525b88d6aaf9b02b2b
kpetigoflorentin7-droid <kpetigoflorentin7@gmail.com>
2026-09-15 13:37:13 +0000

    Configuration du domaine Vercel

--- carte_urgence_project/settings.py
+++ carte_urgence_project/settings.py
@@ -25,10 +25,13 @@ import os
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".vercel.app",
+]
 
 # Application definition
 
